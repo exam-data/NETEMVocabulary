cf1517fe0627929ffcff47e7c5e8ef9fc687b674
Xavi Lee <awxiaoxian2020@163.com>
2023-09-12 21:43:18 +0800

    update with ES module

--- scripts/spelling-variations/lib/index.js
+++ scripts/spelling-variations/lib/index.js
@@ -1,106 +1,141 @@
-const bydictionary = require('./bydictionary.json');
-const bypattern = require('./bypattern.js');
+import bydictionary from './bydictionary.json';
+import bypattern from './bypattern.js';
 
-const spellingVariations = function (word) {
-	this.data = analyse(word);
-};
+class SpellingVariations {
+  constructor(word) {
+    this.data = this.analyse(word);
+  }
 
-// @return {Number} how common this variation in the UK's texts (1-0)
-spellingVariations.prototype.scoreUK = function() {return this.data.scoreUK;};
-// @return {Number} how common this variation in the US's texts (1-0)
-spellingVariations.prototype.scoreUS = function() {return this.data.scoreUS;};
-// @return {Boolean} the word has variations
-spellingVariations.prototype.hasVariations = function() {return this.data.hasVariations;};
-// @return {Array} US variations of the word
-spellingVariations.prototype.USVariations = function() {return this.data.USVariations;};
-// @return {Array} UK variations of the word
-spellingVariations.prototype.UKVariations = function() {return this.data.UKVariations;};
-// @return {String} UK's preferred variation
-spellingVariations.prototype.UKPrefered = function() {return this.data.UKPrefered;};
-// @return {String} US's preferred variation
-spellingVariations.prototype.USPrefered = function() {return this.data.USPrefered;};
-// @return {Array} All of the word's variations
-spellingVariations.prototype.variations = function() {return this.data.variations;};
-// @return {String} UK and US common variation
-spellingVariations.prototype.commonVariation = function() {return this.data.commonVariation;};
-// @return {String} converts the word spelling to it's UK variant
-spellingVariations.prototype.toUK = function() {return this.data.UKPrefered || this.data.word;};
-// @return {String} converts the word spelling to it's US variant
-spellingVariations.prototype.toUS = function() {return this.data.USPrefered || this.data.word;};
-// @return {Object} all the info above
-spellingVariations.prototype.analyse = function() {return this.data;};
-// a us alias for the above function :)
-spellingVariations.prototype.analyze = function() {return this.data;};
+  // @return {Number} how common this variation is in the UK's texts (1-0)
+  scoreUK() {
+    return this.data.scoreUK;
+  }
 
+  // @return {Number} how common this variation is in the US's texts (1-0)
+  scoreUS() {
+    return this.data.scoreUS;
+  }
 
-/**
- * 
- * This little guy here is actually the one who does all the heavy
- * lifting of finding the variations and the class and such..
- * 
-**/
-function analyse(word) {
+  // @return {Boolean} the word has variations
+  hasVariations() {
+    return this.data.hasVariations;
+  }
 
-	word = (word || "").toLowerCase();
+  // @return {Array} US variations of the word
+  USVariations() {
+    return this.data.USVariations;
+  }
 
-	const result = {
-		word,
-		scoreUK:-1,
-		scoreUS:-1,
-		hasVariations:false,
-		UKPrefered:word,
-		USPrefered:word,
-		commonVariation:word,
-		UKVariations:[],
-		USVariations:[],
-		variations:[],
-		analyse:analyse,
-		analyze:analyse
-	};
+  // @return {Array} UK variations of the word
+  UKVariations() {
+    return this.data.UKVariations;
+  }
 
-	var resultArr = [];
-	var dictionaryEntry = bydictionary[word];
-	var patternEntry = bypattern(word);
-	if(dictionaryEntry) resultArr = dictionaryEntry.split("|");
-	else if(patternEntry) resultArr = patternEntry;
-	else return result;
+  // @return {String} UK's preferred variation
+  UKPreferred() {
+    return this.data.UKPreferred;
+  }
 
-	// resultArr reference:
-	// 0: UK1		4: US1
-	// 1: UK2		5: US2
-	// 2: UK3		6: US3
-	// 3: UK4		7: US4		8:UKUS
-	
+  // @return {String} US's preferred variation
+  USPreferred() {
+    return this.data.USPreferred;
+  }
 
-	result.hasVariations = true;
-	result.variations = filterOut(resultArr,word);
-	result.UKPrefered = resultArr[0];
-	result.USPrefered = resultArr[4];
-	result.commonVariation = resultArr[8] || "";
-	result.UKVariations = resultArr.filter((e,i)=>e&&(i<4||i===8)&&e!==word);
-	result.USVariations = resultArr.filter((e,i)=>e&&(i>3||i===8)&&e!==word);
+  // @return {Array} All of the word's variations
+  variations() {
+    return this.data.variations;
+  }
 
-	if(resultArr.indexOf(word) === 8) {
-		result.scoreUK = 0.87;
-		result.scoreUS = 0.87;
-	}
-	
-	else {
-		var UKi = resultArr.slice(0,4).indexOf(word);
-		var USi = resultArr.slice(4,8).indexOf(word);
-		
-		if(UKi === -1) result.scoreUK = 0;
-		else result.scoreUK = (4-UKi)*0.25;
+  // @return {String} UK and US common variation
+  commonVariation() {
+    return this.data.commonVariation;
+  }
 
-		if(USi === -1) result.scoreUS = 0;
-		else result.scoreUS = (4-USi)*0.25;
-	}
+  // @return {String} converts the word spelling to its UK variant
+  toUK() {
+    return this.data.UKPreferred || this.data.word;
+  }
 
-	return result;
+  // @return {String} converts the word spelling to its US variant
+  toUS() {
+    return this.data.USPreferred || this.data.word;
+  }
+
+  // @return {Object} all the info above
+  analyse() {
+    return this.data;
+  }
+
+  // a US alias for the above function :)
+  analyze() {
+    return this.data;
+  }
+
+  /**
+   * 
+   * This little guy here is actually the one who does all the heavy
+   * lifting of finding the variations and the class and such..
+   * 
+  **/
+  analyse(word) {
+    word = (word || "").toLowerCase();
+
+    const result = {
+      word,
+      scoreUK: -1,
+      scoreUS: -1,
+      hasVariations: false,
+      UKPreferred: word,
+      USPreferred: word,
+      commonVariation: word,
+      UKVariations: [],
+      USVariations: [],
+      variations: [],
+      analyse: this.analyse,
+      analyze: this.analyse
+    };
+
+    var resultArr = [];
+    var dictionaryEntry = bydictionary[word];
+    var patternEntry = bypattern(word);
+    if (dictionaryEntry) resultArr = dictionaryEntry.split("|");
+    else if (patternEntry) resultArr = patternEntry;
+    else return result;
+
+    // resultArr reference:
+    // 0: UK1		4: US1
+    // 1: UK2		5: US2
+    // 2: UK3		6: US3
+    // 3: UK4		7: US4		8:UKUS
+
+    result.hasVariations = true;
+    result.variations = this.filterOut(resultArr, word);
+    result.UKPreferred = resultArr[0];
+    result.USPreferred = resultArr[4];
+    result.commonVariation = resultArr[8] || "";
+    result.UKVariations = resultArr.filter((e, i) => e && (i < 4 || i === 8) && e !== word);
+    result.USVariations = resultArr.filter((e, i) => e && (i > 3 || i === 8) && e !== word);
+
+    if (resultArr.indexOf(word) === 8) {
+      result.scoreUK = 0.87;
+      result.scoreUS = 0.87;
+    } else {
+      var UKi = resultArr.slice(0, 4).indexOf(word);
+      var USi = resultArr.slice(4, 8).indexOf(word);
+
+      if (UKi === -1) result.scoreUK = 0;
+      else result.scoreUK = (4 - UKi) * 0.25;
+
+      if (USi === -1) result.scoreUS = 0;
+      else result.scoreUS = (4 - USi) * 0.25;
+    }
+
+    return result;
+  }
+
+  filterOut(arr, word) {
+    return arr.filter((x) => x && x !== word);
+  }
 }
 
-function filterOut(arr,word){
-	return arr.filter((x)=>x&&x!==word);
-}
-
-module.exports = spellingVariations;
+export default SpellingVariations;
