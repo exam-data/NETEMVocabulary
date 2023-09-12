33b5b667846ecc2afc293d754d3a6addc87a7cbd
Xavi Lee <awxiaoxian2020@163.com>
2023-09-12 22:44:22 +0800

    update

---
+++ scripts/spelling-variations/index.js
@@ -0,0 +1,4 @@
+import SpellingVariations from './lib/index.js'
+
+var result = new SpellingVariations("theater").analyze()
+console.log(result)
--- scripts/spelling-variations/lib/index.js
+++ scripts/spelling-variations/lib/index.js
@@ -1,5 +1,24 @@
-import bydictionary from './bydictionary.json';
 import bypattern from './bypattern.js';
+import fs from 'fs';
+
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
+function readJsonFile(filePath) {
+  try {
+    const data = fs.readFileSync(filePath, 'utf8');
+    return JSON.parse(data);
+  } catch (error) {
+    console.error(`Error reading JSON file: ${error}`);
+    return null;
+  }
+}
+
+const bydictionary = readJsonFile(__dirname+'\\bydictionary.json');
 
 class SpellingVariations {
   constructor(word) {
