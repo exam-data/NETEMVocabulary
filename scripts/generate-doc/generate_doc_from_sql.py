cd8b742ca28c4af76ce81e0db0e773bced3e3357
Xavi Lee <xavilee2002@outlook.com>
2023-10-04 17:26:27 +0800

    restore the default logic (#24)
    Co-authored-by: 小杜/ProkingDu <59608902+ProkingDu@users.noreply.github.com>

--- scripts/generate-doc/generate_doc_from_sql.py
+++ scripts/generate-doc/generate_doc_from_sql.py
@@ -3,12 +3,10 @@ from docx import Document
 
 from scripts.custom_config import py_config
 
-
-table = "cet"
-
-if not py_config.original_doc:
-    print("源文档未配置路径！同时请检查数据库配置后运行。")
-    exit(0)
+table = input("请输入要生成文档的表名：")
+if not table:
+    table = "vocabulary"
+    print("未输入表名，默认为vocabulary表。")
 
 # 链接数据库
 Db = pymysql.connect(
@@ -52,31 +50,27 @@ count = 1
 num = 0
 # 单词序号
 
-# 遍历到文档
 for i in range(pageNum):
-    #     # 设定页数
-    table = doc.add_table(rows=py_config.per_num + 1, cols=6)
+# 设定页数
+    table = doc.add_table(rows=py_config.perNum+1, cols=5)
     row = table.rows[0]
-    row.cells[0].text = "序号"
-    row.cells[1].text = "词频"
-    row.cells[2].text = "六级"
-    row.cells[3].text = "单词"
-    row.cells[4].text = "释义"
-    row.cells[5].text = "异形词"
+    row.cells[0].text = '序号'
+    row.cells[1].text = '词频'
+    row.cells[2].text = '单词'
+    row.cells[3].text = '释义'
+    row.cells[4].text = '异形词'
     order = table.columns[0]
     frequency = table.columns[1]
-    cet6 = table.columns[2]
-    word = table.columns[3]
-    meaning = table.columns[4]
-    difference = table.columns[5]
+    word = table.columns[2]
+    meaning = table.columns[3]
+    difference = table.columns[4]
     k = 1
-    for j in range(count, count + py_config.per_num, 1):
+    for j in range(count, count+py_config.per_num, 1):
         order.cells[k].text = str(words_data[num][0])
         frequency.cells[k].text = str(words_data[num][1])
-        cet6.cells[k].text = str(words_data[num][2])
-        word.cells[k].text = str(words_data[num][3])
-        meaning.cells[k].text = str(words_data[num][4])
-        difference.cells[k].text = str(words_data[num][5])
+        word.cells[k].text = str(words_data[num][2])
+        meaning.cells[k].text = str(words_data[num][3])
+        difference.cells[k].text = str(words_data[num][4])
         k = k + 1
         num = num + 1
         if num == total:
