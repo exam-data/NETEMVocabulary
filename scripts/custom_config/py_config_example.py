3d3f648a3404997d2b3f5acabe5834587e00b1b6
Xavi Lee <awxiaoxian2020@163.com>
2023-10-04 21:19:48 +0800

    wip: prepare for generate doc in dynamic way

--- scripts/custom_config/py_config_example.py
+++ scripts/custom_config/py_config_example.py
@@ -4,7 +4,7 @@ database = {
     "host": "127.0.0.1",
     "user": "root",
     "password": "root",
-    "name": 'netem',
+    "name": "netem",
     "port": 3306,
     "charset": "utf-8"
 }
@@ -12,3 +12,12 @@ per_num = 18
 original_doc = ""
 updated_doc = "updated_doc.docx"
 jsonFile = "../../vocabulary.json"
+
+column_list = [
+    {"column_name": "序号", "table_column": "id"},
+    {"column_name": "词频", "table_column": "frequency"},
+    {"column_name": "单词", "table_column": "word"},
+    {"column_name": "释义", "table_column": "definition"},
+    {"column_name": "异形词", "table_column": "variant"}
+]
+table_name = "vocabulary"
