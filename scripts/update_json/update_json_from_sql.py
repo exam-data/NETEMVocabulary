dd975cce626be5ba9118f25c83e94918dadba921
Xavi Lee <xavilee2002@outlook.com>
2023-10-17 11:15:53 +0800

    refactor: only use sql to update json (#27)

--- scripts/update_json/update_json_from_sql.py
+++ scripts/update_json/update_json_from_sql.py
@@ -1,49 +1,54 @@
-import mysql.connector
+import pymysql
 import json
 from scripts.custom_config import py_config
 
-# MySQL数据库连接参数
-db_config = {
-    'host': py_config.database["host"],
-    'user': py_config.database["user"],
-    'password': py_config.database["password"],
-    'database': py_config.database["name"],
-}
+table = py_config.table_name
 
-table = "vocabulary"
+# 链接数据库
+Db = pymysql.connect(
+    host=py_config.database["host"],
+    port=py_config.database["port"],
+    user=py_config.database["user"],
+    password=py_config.database["password"],
+    database=py_config.database["name"],
+)
+column_list = py_config.column_list
 
-# 连接到MySQL数据库
-try:
-    connection = mysql.connector.connect(**db_config)
-    if connection.is_connected():
-        cursor = connection.cursor()
-        
-        # 查询MySQL数据库中的数据，假设 "vocabulary" 表中包含 "word" 和 "variant" 字段
-        cursor.execute(f"SELECT word, variant FROM {table}")
-        data_from_db = cursor.fetchall()
-        
-        # 读取原始 JSON 数据
-        with open('vocabulary.json', 'r', encoding='utf-8') as file:
-            json_data = json.load(file)
-            
-            # 遍历 JSON 数据中的每个词汇条目
-            for entry in json_data['5530考研词汇词频排序表']:
-                word = entry['单词']
-                
-                # 在 MySQL 数据中查找对应单词的 "variant" 值
-                for row in data_from_db:
-                    if row[0] == word:
-                        entry['异形词'] = row[1]
-                        break
-                        
-        # 将更新后的 JSON 数据写回文件
-        with open('vocabulary.json', 'w', encoding='utf-8') as file:
-            json.dump(json_data, file, ensure_ascii=False, indent=4)
-            
-except mysql.connector.Error as error:
-    print(f"Error: {error}")
-    
-finally:
-    if connection.is_connected():
-        cursor.close()
-        connection.close()
+# 2. 获取列名和表字段名的列表
+column_names = [info["column_name"] for info in column_list]
+table_columns = [info["table_column"] for info in column_list]
+
+cursor = Db.cursor()
+
+table_name = table
+
+# 动态生成SELECT查询语句
+select_columns = ", ".join(table_columns)
+query = f"SELECT {select_columns} FROM {table_name}"
+cursor.execute(query)
+
+# 获取查询结果
+data = cursor.fetchall()
+
+# 关闭游标和数据库连接
+cursor.close()
+Db.close()
+
+# 构建JSON数据
+table_data = []
+
+for row in data:
+    row_data = {}
+    for i, column_info in enumerate(column_list):
+        column_name = column_info["column_name"]
+        row_data[column_name] = row[i]  # 使用索引来访问元组中的数据
+    table_data.append(row_data)
+
+json_data = {py_config.table_name_in_json: table_data}
+
+# 将JSON数据写入文件
+output_file = py_config.updated_json_name
+with open(output_file, 'w', encoding='utf-8') as json_file:
+    json.dump(json_data, json_file, ensure_ascii=False, indent=2)
+
+print(f'Data written to {output_file}')
