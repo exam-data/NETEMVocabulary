3fb97936f32e739c8641d3b619646470b481993a
Xavi Lee <xavilee2002@outlook.com>
2024-04-22 14:12:17 +0800

    Apply suggestions from code review

--- netem_full_list.sql
+++ netem_full_list.sql
@@ -2884,7 +2884,7 @@ INSERT INTO `netem_full_list` VALUES (2863, 28, 'brilliant', '闪亮的、聪明
 INSERT INTO `netem_full_list` VALUES (2864, 28, 'gadget', '小玩意儿', NULL, NULL);
 INSERT INTO `netem_full_list` VALUES (2865, 28, 'playground', '操场', NULL, 'shopping');
 INSERT INTO `netem_full_list` VALUES (2866, 28, 'tenant', '房客、租户', NULL, NULL);
-INSERT INTO `netem_full_list` VALUES (2867, 27, 'worthwhile', '与此同时', NULL, NULL);
+INSERT INTO `netem_full_list` VALUES (2867, 27, 'worthwhile', '值得的', NULL, NULL);
 INSERT INTO `netem_full_list` VALUES (2868, 27, 'assert', '断言', NULL, 'communication');
 INSERT INTO `netem_full_list` VALUES (2869, 27, 'mysterious', '神秘的', NULL, NULL);
 INSERT INTO `netem_full_list` VALUES (2870, 27, 'slice', '部分、薄片', NULL, 'food and drink');
