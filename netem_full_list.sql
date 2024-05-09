48ab5812d2c552bd3101036ea1cdd9b8c2c57fbb
knitting-wool-ball <1340430375@qq.com>
2024-05-09 15:44:27 +0800

    update

--- netem_full_list.sql
+++ netem_full_list.sql
@@ -8431,7 +8431,7 @@ INSERT INTO `netem_full_list` VALUES (4205, 9, 'umbrella', '伞', NULL, 'clothes
 
 INSERT INTO `netem_full_list` VALUES (4206, 9, 'verge', '边缘', NULL, NULL);
 
-INSERT INTO `netem_full_list` VALUES (4207, 9, 'aisle', '侧廊', NULL, NULL);
+INSERT INTO `netem_full_list` VALUES (4207, 9, 'aisle', '走廊', NULL, NULL);
 
 INSERT INTO `netem_full_list` VALUES (4208, 9, 'badge', '徽章', NULL, 'clothes');
 
