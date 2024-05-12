1afadf7626f90d603f63a15d2042f87f142c8b73
knitting-wool-ball <85879614+knitting-wool-ball@users.noreply.github.com>
2024-05-12 15:25:30 +0800

    update def of `disposition` (#177)
    Co-authored-by: Xavi Lee <xavilee2002@outlook.com>
    Co-authored-by: rugu <2152732938@qq.com>

--- netem_full_list.sql
+++ netem_full_list.sql
@@ -8511,7 +8511,7 @@ INSERT INTO `netem_full_list` VALUES (4245, 8, 'indignation', '愤怒', NULL, NU
 
 INSERT INTO `netem_full_list` VALUES (4246, 8, 'prudent', '谨慎的', NULL, NULL);
 
-INSERT INTO `netem_full_list` VALUES (4247, 8, 'disposition', '性情、处置', NULL, 'people: personality');
+INSERT INTO `netem_full_list` VALUES (4247, 8, 'disposition', '倾向、安排、性情', NULL, 'people: personality');
 
 INSERT INTO `netem_full_list` VALUES (4248, 8, 'perplex', '使困惑、使复杂化', NULL, NULL);
 
